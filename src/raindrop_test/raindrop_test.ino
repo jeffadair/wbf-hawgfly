e6c03818e6d86e4e7ee9b01a57d32c94840c1e0b
Jeff Adair <jeffadair@senzing.com>
2023-09-20 21:26:27 -0500

    Second strand that mirrors the first; cleanup

--- src/raindrop_test/raindrop_test.ino
+++ src/raindrop_test/raindrop_test.ino
@@ -1,20 +1,15 @@
 #include <FastLED.h>
 #include <ArduinoSTL.h>
-#define DATA_PIN 11
+#define DATA_PIN_LED_STRIP_1 11
+#define DATA_PIN_LED_STRIP_2 9
 #define NUM_LEDS 60
 
 CRGB leds[NUM_LEDS];
 std::vector<CHSV> vec1(NUM_LEDS, CHSV(0, 0, 0));
 
-// std::vector<CHSV> vec1{CHSV(0, 0, 50),
-//                   CHSV(0, 0, 35),
-//                   CHSV(0, 0, 20),
-//                   CHSV(0, 0, 5),
-//                   CHSV(0, 0, 0)};
-
-
 void setup() {
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN_LED_STRIP_1>(leds, NUM_LEDS);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN_LED_STRIP_2>(leds, NUM_LEDS);
   vec1[0] = CHSV(0, 0, 50);
   vec1[1] = CHSV(0, 0, 35);
   vec1[2] = CHSV(0, 0, 20);
