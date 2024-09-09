da62827d316840c1c5eb970df9f79332acbff597
Jeff Adair <jeffadair@senzing.com>
2024-09-09 09:16:14 -0500

    4 drops and using a goldish color

--- src/raindrop_test/raindrop_test.ino
+++ src/raindrop_test/raindrop_test.ino
@@ -13,18 +13,22 @@ void setup() {
   FastLED.addLeds<NEOPIXEL, DATA_PIN_LED_STRIP_1>(leds, NUM_LEDS);
   FastLED.addLeds<NEOPIXEL, DATA_PIN_LED_STRIP_2>(leds, NUM_LEDS);
 
-  drip_vec[7] = CHSV(0, 0, 30);
-  drip_vec[6] = CHSV(0, 0, 60);
-  drip_vec[5] = CHSV(0, 0, 90);
-  drip_vec[4] = CHSV(0, 0, 100);
-  drip_vec[3] = CHSV(0, 0, 80);
-  drip_vec[2] = CHSV(0, 0, 60);
-  drip_vec[1] = CHSV(0, 0, 40);
-  drip_vec[0] = CHSV(0, 0, 20);
+  // H is an angle, 0 to 358, so the angle needs to be mapped to 0-255
+  // goldish 35
+  
+  drip_vec[7] = CHSV(35, 255, 30);
+  drip_vec[6] = CHSV(35, 255, 60);
+  drip_vec[5] = CHSV(35, 255, 90);
+  drip_vec[4] = CHSV(35, 255, 100);
+  drip_vec[3] = CHSV(35, 255, 80);
+  drip_vec[2] = CHSV(35, 255, 60);
+  drip_vec[1] = CHSV(35, 255, 40);
+  drip_vec[0] = CHSV(35, 255, 20);
 
   auto drip1_iter = led_vec.begin();
-  auto drip2_iter = led_vec.begin() + 20;
-  auto drip3_iter = led_vec.begin() + 40;
+  auto drip2_iter = led_vec.begin() + 30;
+  auto drip3_iter = led_vec.begin() + 60;
+  auto drip4_iter = led_vec.begin() + 90;
   for (auto & element : drip_vec) {
     *drip1_iter = element;
     drip1_iter++;
@@ -32,6 +36,8 @@ void setup() {
     drip2_iter++;
     *drip3_iter = element;
     drip3_iter++;
+    *drip4_iter = element;
+    drip4_iter++;
   }
 }
 
